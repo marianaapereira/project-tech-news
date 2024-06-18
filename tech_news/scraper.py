5774c325b9dabb3d6ac2c2bbbf935a564bb52d14
Mariana Pereira <marianapereira.s@live.com>
2024-06-18 20:19:41 -0300

    Implementação da função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
 # Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    raise NotImplementedError
+    time.sleep(1)
+
+    try:
+        response = requests.get(url, timeout=3)
+
+        if response.status_code == 200:
+            return response.text
+
+        else:
+            return None
+
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
